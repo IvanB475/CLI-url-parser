43a270b6cce1e4e28ec0c8a239313e8b248875d5
ivanbeljan <ivanbeljanofficial@gmail.com>
2022-01-13 15:01:03 +0100

    added checks to verify argument's validity

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+import chalk from 'chalk';
+import { readFileSync } from 'fs';
 
 
+//check if argument is provided
 if(process.argv.length < 3) {
-    console.log("please provide path to .txt file")
-    process.kill(process.pid, 'SIGTERM')
+    console.log(chalk.red("please provide path to .txt file"));
+    process.kill(process.pid, 'SIGTERM');
 }
 
 const pathToTxtFile = process.argv[2];
 
+//check if provided argument contains .txt extension
 if(!pathToTxtFile.includes('.txt')) {
-    console.log("Argument provided is not a .txt file")
+    console.log(chalk.red("Argument provided is not a .txt file"));
     process.kill(process.pid, 'SIGTERM')
 }
+
+//check argument was provided with forward slash (on windows) --> Explanation: on windows it's converted to "random" absolute path
+if(pathToTxtFile.includes('C:/')) {
+    console.log(chalk.red("Please do not provide starting forward slash on Windows"));
+    process.kill(process.pid, 'SIGTERM');
+}
+
+const finalPath = pathToTxtFile[0] === '/' ? pathToTxtFile : '/' + pathToTxtFile;
 
