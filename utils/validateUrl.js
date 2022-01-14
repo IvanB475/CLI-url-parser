9cee86d2d7b2db84f94e6421bedcbb38e22e0c4b
ivanbeljan <ivanbeljanofficial@gmail.com>
2022-01-14 15:25:57 +0100

    added validateUrl helper function

---
+++ utils/validateUrl.js
@@ -0,0 +1,5 @@
+export const isValidURL = (string) => {
+    const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+    const urls = string.match(urlRegex);
+    return (urls !== null)
+  };
