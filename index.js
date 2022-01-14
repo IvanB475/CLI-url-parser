3137217c5f748960f7ea35da3fca3084cea8e2a4
ivanbeljan <ivanbeljanofficial@gmail.com>
2022-01-14 15:25:40 +0100

    added read file line by line logic

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import chalk from 'chalk';
-import { readFileSync } from 'fs';
+import * as fs from 'fs';
+import * as readline from 'readline';
+import * as url from 'url';
+import {isValidURL} from './utils/validateUrl.js';
 
 
 //check if argument is provided
@@ -18,9 +21,26 @@ if(!pathToTxtFile.includes('.txt')) {
 
 //check argument was provided with forward slash (on windows) --> Explanation: on windows it's converted to "random" absolute path
 if(pathToTxtFile.includes('C:/')) {
-    console.log(chalk.red("Please do not provide starting forward slash on Windows"));
+    console.log(chalk.red("Please do not provide starting forward slash on Windows without a dot"));
     process.kill(process.pid, 'SIGTERM');
 }
 
-const finalPath = pathToTxtFile[0] === '/' ? pathToTxtFile : '/' + pathToTxtFile;
+const pathWithForwardSlash = pathToTxtFile[0] === '/' || pathToTxtFile[1] === '/' ? pathToTxtFile : '/' + pathToTxtFile;
+const relativePathToTxtFile = pathWithForwardSlash[0] === '.' ? pathWithForwardSlash : '.' + pathWithForwardSlash;
 
+const fileToStream = fs.createReadStream(relativePathToTxtFile);
+
+const rl = readline.createInterface(fileToStream);
+rl.on('line', (input) => {
+    const arrOfStrings = input.split(' ');
+    const urls = arrOfStrings.map((url, index) => {
+        if(isValidURL(url)) return url;
+    })
+
+/*      const x = input.match(urlRegex);
+    console.log(input);
+    console.log("line"); */
+    const filteredUrls = urls?.filter(url => url); 
+    const uniqueUrls = [...new Set(filteredUrls)];
+    console.log(uniqueUrls);
+})
