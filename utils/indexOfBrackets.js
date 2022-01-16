8228cccf316842743f59744823a396042dbbeba9
ivanbeljan <ivanbeljanofficial@gmail.com>
2022-01-16 13:31:00 +0100

    updated indexOfBrackets.js to consider escape characters

--- utils/indexOfBrackets.js
+++ utils/indexOfBrackets.js
@@ -1,22 +1,35 @@
 import chalk from "chalk";
 
-//using match find FIRST match in the given line and return it's index
+// using matchAll find all opening brackets, and then return index of the first one that was not escaped
 export const getIndexOfOpeningBracket = (line) => {
     if(!line) {
         console.log(chalk.red("getIndexOfOpeningBracket requires line to be provided"));
         process.kill(process.pid, 'SIGTERM');
     }
-    const openingBracketIndex = line.match(/\[/)?.index;
-    return openingBracketIndex;
+    const openingBrackets = [...line.matchAll(/\[/g)];
+    let openingBracketIndex;
+    let numberOfEscapedBrackets = 0; 
+    for (let bracket of openingBrackets) {
+        const bracketIndex = bracket.index;
+        if(line[(bracketIndex - 1)] !== '\\') {
+            openingBracketIndex = bracketIndex;
+            break;
+        }
+        numberOfEscapedBrackets++;
+    }
+    const numberOfBracketsToEscape = numberOfEscapedBrackets + 1;
+    const closingBracketIndex = getIndexOfClosingBracket(line, numberOfBracketsToEscape)
+    return {openingBracketIndex, closingBracketIndex};
 }
 
-// using matchAll find all closing brackets, and then return index of the last one
-export const getIndexOfClosingBracket = (line) => {
+
+// using matchAll find all closing brackets, and then return index of specified one
+export const getIndexOfClosingBracket = (line, numberOfEscapedBrackets) => {
     if(!line) {
         console.log(chalk.red("getIndexOfClosingBracket requires line to be provided"));
         process.kill(process.pid, 'SIGTERM');
     }
     const closingBrackets = [...line.matchAll(/\]/g)];
-    const closingBracketIndex = closingBrackets[closingBrackets.length - 1]?.index;
+    const closingBracketIndex = closingBrackets[numberOfEscapedBrackets]?.index;
     return closingBracketIndex;
 }
