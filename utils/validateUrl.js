d103b80653e7c3e86bee5261e1811caa5bf71a44
ivanbeljan <ivanbeljanofficial@gmail.com>
2022-01-16 12:29:19 +0100

    added comment

--- utils/validateUrl.js
+++ utils/validateUrl.js
@@ -1,5 +1,6 @@
-export const isValidURL = (string) => {
+// finds valid urls within given line
+export const isValidURL = (line) => {
     const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-    const urls = string.match(urlRegex);
+    const urls = line.match(urlRegex);
     return (urls !== null)
   };
