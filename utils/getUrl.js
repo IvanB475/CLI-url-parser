16a284d8f3a21f16ee5741f45cd9f7ecd12c24a2
ivanbeljan <ivanbeljanofficial@gmail.com>
2022-01-17 19:35:22 +0100

    moved part of the code from index.js to getUrl.js

---
+++ utils/getUrl.js
@@ -0,0 +1,16 @@
+import { getIndexOfOpeningBracket } from "./indexOfBrackets.js";
+import { isValidUrl } from "./validateUrl.js";
+
+
+export const getUrl = async (line) => {
+    const {openingBracketIndex, closingBracketIndex} = getIndexOfOpeningBracket(line);
+    const inputWithinBrackets = line.substring(openingBracketIndex + 1, closingBracketIndex);
+    const arrOfStrings = inputWithinBrackets.split(" ");
+    const urls = arrOfStrings.map((url) => {
+      if (isValidUrl(url)) return url;
+    });
+
+    const filteredUrls = urls?.filter((url) => url);
+    const urlToMakeRequestTo = filteredUrls[filteredUrls.length - 1];
+    return urlToMakeRequestTo;
+}
